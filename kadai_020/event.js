8aadff2241cd645757f2e97357282457d94aa590
中嶋 <kingof4711@gmail.com>
2024-12-25 09:36:02 +0900

    kadai_020

---
+++ kadai_020/event.js
@@ -0,0 +1,11 @@
+// ボタン要素を取得
+const button = document.getElementById('btn');
+
+// テキスト要素を取得
+const text = document.getElementById('text');
+
+// ボタンがクリックされたときの処理
+button.addEventListener('click',() => {
+  // テキストの内容を変更
+  text.textContent = 'ボタンをクリックしました';
+});
